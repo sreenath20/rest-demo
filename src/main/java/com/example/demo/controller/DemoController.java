02febe06f95f025f95a32d4ddd6d577899217085
sreenath <sreenathbr20@gmail.com>
2022-12-01 16:04:26 +0530

    Employee Map CRUD demo added

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,5 +1,12 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -11,49 +18,61 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Employee;
 
+
 @RestController
+@CrossOrigin(origins ="http://localhost:4200")
 public class DemoController {
 
-	private Employee emp;
+	private Map<Integer,Employee> empMap=new HashMap<>();
+	
 
+	@GetMapping("employees")
+	public Collection<Employee> getAllEmployees() {
+//		List<Employee> empList = new ArrayList<>();
+//		empList.add(new Employee(1,"name 1",100.0));
+//		empList.add(new Employee(4,"name 4",400.0));
+//		empList.add(new Employee(2,"name 2",200.0));
+		return empMap.values();
+	}
 	@GetMapping("/")
+	@CrossOrigin(origins ="http://localhost:4200")
 	public String greeting() {
 		return "Hello Ford";
 	}
 
 	// R
-	@GetMapping("employee")
-	public Employee getEmployee() {
-		return this.emp;
+	@GetMapping("employee/{id}")
+	public Employee getEmployee(@PathVariable("id") Integer id) {
+		return this.empMap.get(id);
 	}
 
 	// C
 	@PostMapping("employee")
 	public String addEmployee(@RequestBody Employee newEmployee) {
-		emp = newEmployee;
-		System.out.println(newEmployee);
+		
+		this.empMap.put(newEmployee.getId(),newEmployee);
 		return "Employee added";
 	}
 
 	//U
 	@PutMapping("employee")
 	public String updateEmployee(@RequestBody Employee updateEmployee) {
-		emp = updateEmployee;
-		System.out.println(updateEmployee);
+		
+		this.empMap.replace(updateEmployee.getId(),updateEmployee);
 		return "Employee Updated";
 	}
 
 	//D
-	@DeleteMapping("employee")
-	public String deleteEmployee() {
-		this.emp = null;
-		return "Employee Delete";
+	@DeleteMapping("employee/{id}")
+	public String deleteEmployee(@PathVariable("id") Integer id) {
+		this.empMap.remove(id);
+		return "Employee Deleted by id="+id;
 	}
 	//Partial update of resource
-	@PatchMapping("employee/name/{name}")
-	public String updateEmployeeName(@PathVariable("name") String newName) {
+	@PatchMapping("employee/name/{id}/{name}")
+	public String updateEmployeeName(@PathVariable("id") Integer id, @PathVariable("name") String newName) {
 		
-		emp.setName(newName);
+		this.empMap.get(id).setName(newName);
 		return "Employee name updated";
 	}
 
